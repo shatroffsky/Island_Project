d32c693e11755950647b3aadedd5738c129abf3c
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-07-05 17:52:41 +0300

    Add abstract Predator and Herbivore classes extending Animal to categorize animals.

--- src/model/Herbivore.java
+++ src/model/Herbivore.java
@@ -1,4 +1,10 @@
 package model;
 
-public class Herbivore {
+import map.Cell;
+
+public abstract class Herbivore extends Animal {
+
+    public Herbivore(int weight, int maxCountInCell, int speed, double foodNeed, Cell cell) {
+        super(weight, maxCountInCell, speed, foodNeed, cell);
+    }
 }
--- src/model/Predator.java
+++ src/model/Predator.java
@@ -1,4 +1,9 @@
 package model;
 
-public class Predator {
+import map.Cell;
+
+public abstract class Predator extends Animal {
+    public Predator(int weight, int maxCountInCell, int speed, int saturation, Cell cell) {
+        super(weight, maxCountInCell, speed, saturation, cell);
+    }
 }
