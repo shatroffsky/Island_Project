07bc9da8576a7679b7edc23a31ec61eb42e2c105
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-07-05 17:50:30 +0300

    Add Cell class to manage coordinates and lists of animals and plants with methods to add and remove them.

--- src/map/Cell.java
+++ src/map/Cell.java
@@ -1,4 +1,56 @@
 package map;
 
+import model.Animal;
+import model.Plant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
+    private final int x;
+    private final int y;
+
+    private final List<Animal> animals = new ArrayList<>();
+    private final List<Plant> plants = new ArrayList<>();
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Додати тварину
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    // Видалити тварину
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
+
+    // Додати рослину
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    // Видалити рослину
+    public void removePlant(Plant plant) {
+        plants.remove(plant);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
+    }
 }
