659caa6238147350f1187f3e040185f862620ffb
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-07-05 17:53:01 +0300

    Add Plant class with properties for weight, max count per cell, and current cell location.

--- src/model/Plant.java
+++ src/model/Plant.java
@@ -1,4 +1,26 @@
 package model;
 
+import map.Cell;
+
 public class Plant {
+
+    protected int weight = 1;
+    protected int maxCountInCell = 200;
+    protected Cell currentCell;
+
+    public Plant(Cell currentCell) {
+        this.currentCell = currentCell;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getMaxCountInCell() {
+        return maxCountInCell;
+    }
+
+    public Cell getCurrentCell() {
+        return currentCell;
+    }
 }
